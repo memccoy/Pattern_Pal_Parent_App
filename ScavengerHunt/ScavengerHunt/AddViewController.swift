27d4dea2f461a89be308f184780a4b36137b80ad
kfeav <kfeav@umich.edu>
2015-10-03 10:25:52 -0400

    Adding ScavengerHunt App

---
+++ ScavengerHunt/ScavengerHunt/AddViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AddViewController.swift
+//  ScavengerHunt
+//
+//  Created by Katy Feaver on 9/10/15.
+//  Copyright (c) 2015 Katy Feaver. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+        
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "DoneItem" {
+            if let name = textField.text {
+                if !name.isEmpty {
+                    newItem = ScavengerHuntItem(name: name)
+                }
+            }
+        }
+    }
+    
+    var newItem: ScavengerHuntItem?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ ScavengerHunt/ScavengerHunt/ScavengerHuntItem.swift
@@ -0,0 +1,40 @@
+//
+//  ScavengerHuntItem.swift
+//  ScavengerHunt
+//
+//  Created by Katy Feaver on 9/10/15.
+//  Copyright (c) 2015 Katy Feaver. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ScavengerHuntItem: NSObject, NSCoding {
+    
+    let name: String
+    var photo: UIImage?
+    var isCompleted: Bool {
+        get {
+            return photo != nil
+        }
+    }
+    
+    let nameKey = "name"
+    let photoKey = "photo"
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: nameKey)
+        if let thePhoto = photo {
+            aCoder.encodeObject(thePhoto, forKey: photoKey)
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObjectForKey(nameKey) as String
+        photo = aDecoder.decodeObjectForKey(photoKey) as? UIImage
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+}
